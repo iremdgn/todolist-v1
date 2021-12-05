c6fc2410f66a275281555b650d425ce2ca74efb0
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-05 19:47:16 +0300

    add ejs

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    
-    var today = new Date();
-    var currentDay = today.getDate();
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.send("Yay! It's the weekend!");
-    } else {
-        res.send("Boo! I have to work!");
-    }
+    const today = new Date();
+
+    const weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var currentDay = weekday[today.getDay()];
+
+    res.render('list', { kindOfDay: currentDay });
+ 
 });
 
 app.listen(3000, function () {
