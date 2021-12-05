6f2aa080f5190be70ad9db1ee22b248a3a400e54
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-05 22:10:09 +0300

    update

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.get('/', function (req, res) {
 
     const today = new Date();
@@ -16,12 +20,29 @@ app.get('/', function (req, res) {
     weekday[4] = "Thursday";
     weekday[5] = "Friday";
     weekday[6] = "Saturday";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
     var currentDay = weekday[today.getDay()];
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render('list', { kindOfDay: day , newListItems: items });
 
     res.render('list', { kindOfDay: currentDay });
  
 });
+
+app.post('/', function (req, res) {
+
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log('listening on port 3000');
