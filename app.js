c8d6cf372bdf0aa35bcba6bc3697671ae3a7dbaa
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-06 20:59:49 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -7,21 +8,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+var items = [];
 var workItems = [];
 
 app.get('/', function (req, res) {
 
-    const today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    let day = date.getDate();
     res.render('list', { listTitle: day, newListItems: items });
 
 });
@@ -46,6 +38,9 @@ app.get('/work', function (req, res) {
     res.render('list', { listTitle:"Work List", newListItems: workItems });
 });
 
+app.get('/about', function (req, res) {
+    res.render('about');
+})
 
 app.listen(3000, function () {
     console.log('listening on port 3000');
