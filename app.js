496b3efe301dc8c1de2b033804662ed3f3f14fff
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-27 11:48:49 +0300

    update

--- app.js
+++ app.js
@@ -16,27 +16,58 @@ const itemsSchema = {
 
 const Item = mongoose.model('Item', itemsSchema);
 
+const item1 = new Item ({
+    name: "Welcome to your To Do List!"
+});
+
+const item2 = new Item ({
+    name: "Hit + button to add a new item!"
+});
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item!"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 var items = [];
 var workItems = [];
 
 app.get('/', function (req, res) {
 
-    res.render('list', { listTitle: "Today", newListItems: items });
+    Item.find({}, function (err, foundItems) {
+
+        if ( foundItems.length === 0) {
+
+            Item.insertMany(defaultItems, function (err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved default items DB.");
+                }
+            }); 
+            res.redirect('/');
+        } else {
+            res.render('list', { listTitle: "Today", newListItems: foundItems });
+        }
+       
+    });
 
 });
 
 app.post('/', function (req, res) {
 
-    let item = req.body.newItem;
+    const itemName = req.body.newItem;
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
+    const item = new Item ({
+        name: itemName
+    });
 
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
+    item.save();
+
+    res.redirect('/');
 
 });
 
