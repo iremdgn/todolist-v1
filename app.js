571e906d834a335f11519246dec0d9521a58b683
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-12-28 13:11:18 +0300

    Intial commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin-irem:Blahfark123@cluster0.zjk3k.mongodb.net/todolistDB', { useNewUrlParser: true });
 
 const itemsSchema = {
     name: String
